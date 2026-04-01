460ed89be4d78e2975ae9d9d05c90cdc6a032f50
rexdarenlco-eng <rex.darnel.co@gmail.com>
2026-04-01 08:36:51 +0530

    Finalized branding and automation script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo "Enter your Email:"
 read email
 
 # This replaces the placeholder text in index.html
-sed -i "s/FREE<span class=\"text-cyan-400\">LANCE./$name/g" index.html
+sed -i "s/FREE<span class=\"text-cyan-400\">LANCER./$name/g" index.html
 sed -i "s/your-email@example.com/$email/g" index.html
 
 echo "Success! Your site is now personalized for $name."
